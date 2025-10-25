cc903be395d7b47040f4714fa0248ba1669b3761
wonjjani <wonjjani@gmail.com>
2025-10-25 12:37:32 +0900

    feat: haha

--- setup/set-front.sh
+++ setup/set-front.sh
@@ -4,8 +4,10 @@ if [ -n "$PID" ]; then
   echo "포트 8200 ㅅ용 중 -> PID: $PID 종료"
   sudo kill -9 $PID
 else
-  git clone https://github.com/BOB-DSPM/SAGE-FRONT
-  cd SAGE-FRONT/dspm_dashboard
-  npm install
-  HOST=0.0.0.0 PORT=8200 npm start
+  pass
 fi
+
+git clone https://github.com/BOB-DSPM/SAGE-FRONT
+cd SAGE-FRONT/dspm_dashboard
+npm install
+HOST=0.0.0.0 PORT=8200 npm start
