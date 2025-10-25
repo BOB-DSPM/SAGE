99fcb48d8bb7f67f6bfe9c8abbf891fc251bb2bd
wonjjani <wonjjani@gmail.com>
2025-10-25 12:51:01 +0900

    feat: haha

--- setup/set-front.sh
+++ setup/set-front.sh
@@ -1,14 +1,20 @@
+#!/usr/bin/env bash
 set -ex
-PID=$(lsof -ti tcp:8200)
+
+PID=$(lsof -ti tcp:8200 || true)
 
 if [ -n "$PID" ]; then
-  echo "포트 8200 ㅅ용 중 -> PID: $PID 종료"
+  echo "포트 8200 사용 중 -> PID: $PID 종료"
   sudo kill -9 $PID
 else
   echo "포트 8200 사용 중인 프로세스 없음"
 fi
 
-git clone https://github.com/BOB-DSPM/SAGE-FRONT
+# 이미 클론되어 있으면 다시 안 받도록 처리
+if [ ! -d "SAGE-FRONT" ]; then
+  git clone https://github.com/BOB-DSPM/SAGE-FRONT
+fi
+
 cd SAGE-FRONT/dspm_dashboard
 npm install
 HOST=0.0.0.0 PORT=8200 npm start
