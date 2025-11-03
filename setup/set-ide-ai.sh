cb9d639265c58a3289ee0c331a01419d96286a23
wonjjani <wonjjani@gmail.com>
2025-11-03 19:02:21 +0900

    Feat:Ahah

--- setup/set-ide-ai.sh
+++ setup/set-ide-ai.sh
@@ -21,5 +21,5 @@ sudo apt install wget tar
 wget https://github.com/BOB-DSPM/SAGE_Identity-AI/releases/download/v0.1.0/xlmr-large-min.tar.zst
 ls
 tar --zstd -xf xlmr-large-min.tar.zst 
-
+export MODEL_DIR=./xlmr-large-min
 nohup python -m uvicorn app.main:app --host 0.0.0.0 --port 8900 > iden-ai.log 2>&1 & echo $! > iden-ai.pid
