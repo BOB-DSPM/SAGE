9cef48e02cfca9853e5d98c859c4f4d7696fd7e2
wonjjani <wonjjani@gmail.com>
2025-11-09 14:32:21 +0900

    Feat:haha

--- setup/set-analyzer.sh
+++ setup/set-analyzer.sh
@@ -7,11 +7,17 @@ else
   echo "포트 9000 사용 중인 프로세스 없음"
 fi
 
-git clone https://github.com/BOB-DSPM/DSPM_DATA-Identification-Classification/tree/analyzer
-cd DSPM_DATA-Identification-Classification/dspm-analyzer
+# 1) 레포 클론
+git clone --branch analyzer --single-branch https://github.com/BOB-DSPM/DSPM_DATA-Identification-Classification.git DSPM_DATA-IC-analyzer
+cd DSPM_DATA-IC-analyzer
 
+# 2) 가상환경 생성 및 활성화
 python3 -m venv .venv
+# macOS/Linux
 source .venv/bin/activate
+
+# 3) 필수 패키지 설치
+cd dspm-analyzer
 pip install -r requirements.txt
 pip install fastapi uvicorn[standard]
 
